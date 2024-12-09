7f462cfc0e9f3b67b69e2bdd86513d57a7d7b92e
DenisEliseev1 <khandagay@gmail.com>
2024-12-10 00:42:58 +0800

    version 1.3.1  + send children and files method

--- FamilyTree.java
+++ FamilyTree.java
@@ -15,12 +15,13 @@ public class FamilyTree {
             ages = person.getAges();
             sName = person.getSurname();
             FileWriter file = new FileWriter (name+sName+ages+".txt");
-            file.write ("Родитель: " + person + "\n" + "Дети:");
+            file.write ("Предок: " + person + "\n" + "Потомки:");
             file.append('\n');
             for (Person person2 : pers) {
                 if ((sName == person2.getSurname())&&((name == person2.getNameFather())||(name == person2.getNameMother())) 
                 && ((ages - person2.getAges()) >= 18)){
                     file.append ("-" + person2);
+                    file.append('\n');
                 }
 
             }
